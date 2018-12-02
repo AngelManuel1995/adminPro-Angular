b5de0dcbc6efec577c3f726bc30600aca19bae1b
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2018-12-02 09:15:13 -0500

    User fomulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { PagesModule } from './pages/pages.module'
 import { APP_ROUTES } from './app.routes'
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { LoginComponent, } from './login/login.component'
@@ -19,6 +19,8 @@ import { RegisterComponent } from "./login/register.component";
     BrowserModule,
     APP_ROUTES,
     PagesModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
   ],
--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core"
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector:'app-register',
@@ -7,10 +8,24 @@ import { Component, OnInit } from "@angular/core"
 })
 
 export class RegisterComponent implements OnInit{
+    
+    forma: FormGroup
+
     constructor(){
 
     }
+
     ngOnInit(): void {
-    
+        this.forma = new FormGroup({
+            nombre: new FormControl(null, Validators.required),
+            correo: new FormControl(null, [Validators.required, Validators.email]),
+            password: new FormControl(null, Validators.required),
+            password2: new FormControl(null, Validators.required),
+            condiciones: new FormControl(false),
+        });
+    }
+    registrarUsuario(){
+        
+        console.log(this.forma.value)
     }
 }
